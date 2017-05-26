78e5f7548e9f054db42ae7543aa01a214b53b0d0
fuyun <1440030538@qq.com>
2017-05-26 23:14:57 +0800

    add

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,12 +2,15 @@
 namespace app\index\controller;
 use app\common\model\MBokearticle;
 use think\Request;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
 
     public function index()
     {
+    	$this->redirect('/index/people/index');
+
         $list = (new MBokearticle())->getList(['category_id'=>9,'page'=>Request::instance()->get('page',1)]);
 
         return view('index',['list'=> $list]);
